e9be26242ffc77f8de4914214eb180221ccb9087
Dominic Montelongo <padma@email.com>
2020-04-07 15:09:35 -0700

    Add 'this.currentId' to addressBook constructor to maintain unique identification for each contact object

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,14 @@
 //Business Logic for AddressBook // AddressBook (AB) contains a single property: an empty array called "contacts"
 function AddressBook() {
     this.contacts = [];
+    // Add an uniqueID  to record an unique ID
+    this.currentId = 0;
   }
 
 AddressBook.prototype.addContact = function(contact) {
     this.contacts.push(contact);
 }
-// Business Logic for Contacts  // "contacts"Objects
+// Business Logic for Contacts  // "contacts" Objects
 function Contact(firstName, lastName, phoneNumber) {
     this.firstName = firstName;
     this.lastName = lastName;
@@ -16,3 +18,5 @@ function Contact(firstName, lastName, phoneNumber) {
   Contact.prototype.fullName = function() {
     return this.firstName + " " + this.lastName;
   }
+
+
