777dc44a5f7076b2e9a7b28dd470a36074c9d3bc
Dominic Montelongo <padma@email.com>
2020-04-07 11:43:22 -0700

    Comment out descriptions for AddressBook and Contact along with pointing out where the business logic is for AB and Contacts

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
+//Business Logic for AddressBook // AddressBook (AB) contains a single property: an empty array called "contacts"
 function AddressBook()  {
     this.contacts = [];
 }
+// Business Logic for Contacts  // "contacts"Objects
 function Contact(firstName, lastName, phoneNumber) {
     this.firstName = firstName;
     this.lastName = lastName;
