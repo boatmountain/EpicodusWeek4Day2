552b72fb55baf7099e2c3a914339e38aa4271d4a
Dominic Montelongo <padma@email.com>
2020-04-07 15:14:17 -0700

    Add prototype.assignId to identify each contact property with unique identifier

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,17 @@
-//Business Logic for AddressBook // AddressBook (AB) contains a single property: an empty array called "contacts"
+//Business Logic for AddressBook // AddressBook is a constructor (AB) contains a single property: an empty array called "contacts"
 function AddressBook() {
     this.contacts = [];
     // Add an uniqueID  to record an unique ID
     this.currentId = 0;
   }
 
-AddressBook.prototype.addContact = function(contact) {
+AddressBook.prototype.addContact = function(contact)  {
     this.contacts.push(contact);
+}
+// This new method will increment the 'this.currentId' property on the AD obj. by 1 and return the updated value.  This makes themunique by not repeating
+AddressBook.prototype.assignId = function ()  {
+    this.currentId += 1;
+    return this.currentId;
 }
 // Business Logic for Contacts  // "contacts" Objects
 function Contact(firstName, lastName, phoneNumber) {
