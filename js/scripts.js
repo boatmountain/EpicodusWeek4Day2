738586d68a8c037d0452c3644c88020f1906621e
Dominic Montelongo <padma@email.com>
2020-04-07 20:14:09 -0700

    Add 'prototype.findContact' to locate contactId in AB

--- js/scripts.js
+++ js/scripts.js
@@ -10,11 +10,27 @@ AddressBook.prototype.addContact = function(contact)  {
     contact.id = this.assignId()
     this.contacts.push(contact);    
 }
+
 // This new method will increment the 'this.currentId' property on the AD obj. by 1 and return the updated value.  This makes themunique by not repeating
 AddressBook.prototype.assignId = function ()  {
     this.currentId += 1;
     return this.currentId;
 }
+
+// This new method will find a specific contact in AB
+// Id is taken as an argument because it will contain the unique ID assigned to each contact
+AddressBook.prototype.findContact = function(id) {
+// The method loops through the AB obj contacts array checking entry id against id provided to the "prototype.findContact"
+// 'For loop' is used because it can break out unlike a 'for each loop'
+    for (var i=0; i <this.contacts.length; i++) {
+// When match is found, the method is return
+        if(this.contacts[i].id==id) {
+            return this.contacts[i];
+        }
+    };
+    return false
+}
+
 // Business Logic for Contacts  // "contacts" Objects
 function Contact(firstName, lastName, phoneNumber) {
     this.firstName = firstName;
