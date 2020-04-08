4be796340b0c6a9003295a3200c704b68547ef07
Dominic Montelongo <padma@email.com>
2020-04-07 21:55:17 -0700

    Add delete contact function that includes for loop to identify contact id that matches entered id

--- js/scripts.js
+++ js/scripts.js
@@ -12,23 +12,35 @@ AddressBook.prototype.addContact = function(contact)  {
 }
 
 // This new method will increment the 'this.currentId' property on the AD obj. by 1 and return the updated value.  This makes themunique by not repeating
-AddressBook.prototype.assignId = function ()  {
+AddressBook.prototype.assignId = function()  {
     this.currentId += 1;
     return this.currentId;
 }
 
 // This new method will find a specific contact in AB
 // Id is taken as an argument because it will contain the unique ID assigned to each contact
-AddressBook.prototype.findContact = function(id) {
+AddressBook.prototype.findContact = function(id)  {
 // The method loops through the AB obj contacts array checking entry id against id provided to the "prototype.findContact"
 // 'For loop' is used because it can break out unlike a 'for each loop'
-    for (var i=0; i <this.contacts.length; i++) {
+    for (var i=0; i < this.contacts.length; i++) {
 // When match is found, the method is return
-        if(this.contacts[i].id==id) {
+        if(this.contacts[i].id == id) {
             return this.contacts[i];
         }
     };
     return false
+}
+//This is a new method that will delete an existing contact or return false if contact doesn't exist
+//Id is taken as an argument because it will conain the unique id assigned to each conact
+AddressBook.prototype.deleteContact = function(id)  {
+//This method loops through the AB obj contacts array checking entry id against id provided to the "prototype.deleteContact"
+    for (var i=0; i < this.contacts.length; i++)  {
+        if (this.contacts[i].id == id) {
+            delete this.contacts[i];
+            return true;
+        }
+    };
+    return false;
 }
 
 // Business Logic for Contacts  // "contacts" Objects
